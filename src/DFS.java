205de8f88ed8115f85ed1f00e8f60233da423cca
amosalah <amrmohamed81298@gmail.com>
2023-09-19 17:22:29 +0300

    changed the view

--- src/DFS.java
+++ src/DFS.java
@@ -1,48 +1,60 @@
 import java.util.List;
 
 public class DFS {
-    public static boolean searchPath(int[][] maze, int x, int y
-            , List<Integer> path) {
-        if (maze[y][x] == 9) {
-            path.add(x);
-            path.add(y);
+
+    public static boolean searchPath(int[][] maze, List<Integer> path, int curX, int curY) {
+        if(maze[curX][curY] == 9) {
+            path.add(curX);
+            path.add(curY);
             return true;
         }
 
-        if (maze[y][x] == 0) {
-            maze[y][x] = 2;
+        if(maze[curX][curY]==0) {
+            maze[curX][curY]=2;
+            int dx,dy;
 
-            int dx = -1;
-            int dy = 0;
-            if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+            dx=0;
+            dy=-1;
+            if(searchPath(maze,path,curX+dx,curY+dy)) {
+                path.add(curX);
+                path.add(curY);
                 return true;
             }
 
-            dx = 1;
-            dy = 0;
-            if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+            dx=0;
+            dy=1;
+            if(searchPath(maze,path,curX+dx,curY+dy)) {
+                path.add(curX);
+                path.add(curY);
                 return true;
             }
 
-            dx = 0;
-            dy = -1;
-            if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+            dx=-1;
+            dy=0;
+            if(searchPath(maze,path,curX+dx,curY+dy)) {
+                path.add(curX);
+                path.add(curY);
                 return true;
             }
 
-            dx = 0;
-            dy = 1;
-            if (searchPath(maze, x + dx, y + dy, path)) {
-                path.add(x);
-                path.add(y);
+            dx=1;
+            dy=0;
+
+            //getSearchPath(maze,path,curX,curY,dx,dy);
+            if(searchPath(maze,path,curX+dx,curY+dy)) {
+                path.add(curX);
+                path.add(curY);
                 return true;
             }
+        }
+        return false;
+    }
+
+    private static boolean getSearchPath(int[][] maze, List<Integer> path, int curX, int curY, int dx, int dy) {
+        if(searchPath(maze,path,curX+dx,curY+dy)) {
+            path.add(curX);
+            path.add(curY);
+            return true;
         }
         return false;
     }
--- src/View.java
+++ src/View.java
@@ -14,7 +14,7 @@ public class View extends JFrame {
                 {1,0,1,0,1,1,1,0,1,0,0,0,1},
                 {1,0,1,0,1,0,0,0,1,1,1,0,1},
                 {1,0,1,0,1,1,1,0,1,0,1,0,1},
-                {1,0,0,0,0,0,0,0,0,0,1,9,1},
+                {1,0,0,0,0,9,0,0,0,0,1,0,1},
                 {1,1,1,1,1,1,1,1,1,1,1,1,1}
             };
 
@@ -27,7 +27,7 @@ public class View extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-        DFS.searchPath(maze,1,1,path);
+        DFS.searchPath(maze,path,1,1);
         System.out.println(path);
     }
     @Override
@@ -38,16 +38,15 @@ public class View extends JFrame {
         //start coloring the maze
         for (int row = 0; row < maze.length; row++) {
             for (int col = 0; col < maze[0].length; col++) {
-                Color color = Color.WHITE;
+                Color color;
                 switch (maze[row][col])
                 {
-                    //case 0: color=Color.WHITE; break;
                     case 1: color=Color.BLACK; break;
-                    //case 2: color=Color.GREEN; break;
                     case 9: color=Color.RED; break;
                     default : color = Color.WHITE;
 
                 }
+                //delay before drawing each cell in the whole maze
                 makeDelay(10);
                 g.setColor(color);
                 g.fillRect(30*col,30*row,30,30);
@@ -56,22 +55,32 @@ public class View extends JFrame {
                 g.drawRect(30*col,30*row,30,30);
             }
         }
-        //starting from the end point(11,8) to the start point(1,1)
-        /*for(int p=0;p<path.size();p+=2) {
+        Collections.reverse(path);
+        for(int p=0;p<path.size();p+=2) {
             makeDelay(200);
             int pathX = path.get(p);
             int pathY = path.get(p+1);
             g.setColor(Color.GREEN);
             g.fillRect(pathX*30,pathY*30,30,30);
-        }*/
-        for(int p=path.size()-1;p>=0;p-=2) {
+        }
+        //this commented code starting from the end point(11,8) to the start point(1,1)
+        /*for(int p=path.size()-1;p>=0;p-=2) {
             makeDelay(200);
             int pathX = path.get(p-1);
             int pathY = path.get(p);
-            g.setColor(Color.GREEN);
+            g.setColor(Color.YELLOW);
             g.fillRect(pathX*30,pathY*30,30,30);
-        }
+        }*/
 
+    }
+
+    public static void makeDelay(int delay) {
+        try {
+            Thread.sleep(delay);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -83,14 +92,5 @@ public class View extends JFrame {
             }
         });
     }
-    public static void makeDelay(int delay)
-    {
-        try {
-            Thread.sleep(delay);
-        }
-        catch (InterruptedException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
+
 }
